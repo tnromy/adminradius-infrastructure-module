e721eca9199b47124cdc6474efd25466f5c402aa
tnromy <tuanromy@gmail.com>
2025-09-27 08:30:44 +0700

    memperbaiki routing device connection

--- src/routes/device_connection_route.rs
+++ src/routes/device_connection_route.rs
@@ -4,11 +4,19 @@ use crate::controllers::device_connection_controller as controller;
 
 pub fn configure(cfg: &mut ServiceConfig) {
     cfg.service(
-        web::scope("/device/{device_id}")
-            .route("/connections", web::get().to(controller::index))
-            .route("/connection", web::post().to(controller::store))
-            .route("/connection/{connection_id}", web::get().to(controller::show))
-            .route("/connection/{connection_id}", web::put().to(controller::update))
-            .route("/connection/{connection_id}", web::delete().to(controller::destroy)),
+        web::resource("/device/{device_id}/connections")
+            .route(web::get().to(controller::index)),
+    );
+
+    cfg.service(
+        web::resource("/device/{device_id}/connection")
+            .route(web::post().to(controller::store)),
+    );
+
+    cfg.service(
+        web::resource("/device/{device_id}/connection/{connection_id}")
+            .route(web::get().to(controller::show))
+            .route(web::put().to(controller::update))
+            .route(web::delete().to(controller::destroy)),
     );
 }
--- src/routes/device_port_route.rs
+++ src/routes/device_port_route.rs
@@ -4,11 +4,19 @@ use crate::controllers::device_port_controller as controller;
 
 pub fn configure(cfg: &mut ServiceConfig) {
     cfg.service(
-        web::scope("/device/{device_id}")
-            .route("/ports", web::get().to(controller::index))
-            .route("/port", web::post().to(controller::store))
-            .route("/port/{port_id}", web::get().to(controller::show))
-            .route("/port/{port_id}", web::put().to(controller::update))
-            .route("/port/{port_id}", web::delete().to(controller::destroy)),
+        web::resource("/device/{device_id}/ports")
+            .route(web::get().to(controller::index)),
+    );
+
+    cfg.service(
+        web::resource("/device/{device_id}/port")
+            .route(web::post().to(controller::store)),
+    );
+
+    cfg.service(
+        web::resource("/device/{device_id}/port/{port_id}")
+            .route(web::get().to(controller::show))
+            .route(web::put().to(controller::update))
+            .route(web::delete().to(controller::destroy)),
     );
 }
